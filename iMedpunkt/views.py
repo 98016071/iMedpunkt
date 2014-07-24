2cba0d7cc9ef03b0fdf6ffc4285d8a5342e3972a
Pavel Mavrin <pavel.mavrin@gmail.com>
2014-07-24 20:05:40 +0400

    Krasota

--- iMedpunkt/views.py
+++ iMedpunkt/views.py
@@ -30,9 +30,6 @@ def student_post(request, student_id):
         return HttpResponse('Fuck you!')
     data = request.POST
     stud = Student.objects.get(pk=student_id)
-    x = open('hren.txt', 'w')
-    print(data['cert1'], x)
-    x.close()
     stud.cert1, stud.cert2, stud.health, stud.allergy = data.get('cert1', False), data.get('cert2', False), data['health'], data['allergy']
     stud.bolel, stud.height, stud.weight = data['bolel'], data['height'], data['weight']
     stud.save()
