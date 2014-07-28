573416a57fd56b6295632b63c2b96c671bf1cd80
98016071 <al_morozoff@outlook.com>
2014-07-28 18:26:06 +0400

    Added DatePicker and TimePicker

--- iMedpunkt/urls.py
+++ iMedpunkt/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, url
-from iMedpunkt.views import student_list, root, student, student_post, visits_list, visit_edit, visit_post, visit_new, login, \
-date_edit
-
+from iMedpunkt.views import student_list, root, student, student_post, visits_list, visit_edit, visit_post, visit_new, login
 urlpatterns = patterns('',
     url(r'^student/(?P<student_id>\d+)$', student, name='student'),
     url(r'^student_list$', student_list, name='student_list'),
@@ -10,6 +8,5 @@ urlpatterns = patterns('',
     url(r'^visit_edit/(?P<visit_id>\d+)$', visit_edit, name='visit_edit'),
     url(r'^visit_post/(?P<visit_id>\d+)$', visit_post, name='visit_post'),
     url(r'^visit_new/(?P<student_id>\d+)$', visit_new, name='visit_new'),
-    url(r'^date_edit/(?P<visit_id>\d+)$', date_edit, name='date_edit'),
     url(r'^$', root, name='root')
 )
